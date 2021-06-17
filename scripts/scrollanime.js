fc6fe8dd1ddf36b464692f95b746eac467ec9f70
momongya <mo5e2ka8@gmail.com>
2021-06-18 03:39:44 +0900

    SP版しおり傾くアニメーション完

--- scripts/scrollanime.js
+++ scripts/scrollanime.js
@@ -23,15 +23,63 @@ function scroll_effect(){
     var tt = $(window).scrollTop();
     var hh = $(window).height();
     $('.scroll-up').each(function(){
-        var yy = $(this).offset().top+hh/50;//効果発生開始タイミングを操作したい場合は数値を変更する
+        if (window.matchMedia( "(max-width: 768px)" ).matches) {
+            var yy = $(this).offset().top+hh/2;//効果発生開始タイミングを操作したい場合は数値を変更する
+            if (tt > yy - hh){
+                $(this).addClass('done');
+            }
+        }
+        if (window.matchMedia( "(min-width: 768px)" ).matches) {
+            var yy = $(this).offset().top+400;//効果発生開始タイミングを操作したい場合は数値を変更する
+            if (tt > yy - hh){
+                $(this).addClass('done');
+            }
+        }
+    });
+
+    // しおりが傾くアニメーション (右側たち)
+    $('.salaryspchara').each(function(){
+        var yy = $(this).offset().top+hh/2;//効果発生開始タイミングを操作したい場合は数値を変更する
         if (tt > yy - hh){
-            $(this).addClass('done');
+            $(this).css( { transform: "rotate(7deg)" } );
+            $(this).css( { "transition-timing-function" : "cubic-bezier(.83,0,.22,1)" } );
         }
     });
     $('.nursespimg').each(function(){
-        var yy = $(this).offset().top+hh/3;//効果発生開始タイミングを操作したい場合は数値を変更する
+        var yy = $(this).offset().top+hh/2;//効果発生開始タイミングを操作したい場合は数値を変更する
         if (tt > yy - hh){
             $(this).css( { transform: "rotate(7deg)" } );
+            $(this).css( { "transition-timing-function" : "cubic-bezier(.83,0,.22,1)" } );
+        }
+    });
+    $('.busgirlspchara').each(function(){
+        var yy = $(this).offset().top+hh/2;//効果発生開始タイミングを操作したい場合は数値を変更する
+        if (tt > yy - hh){
+            $(this).css( { transform: "rotate(7deg)" } );
+            $(this).css( { "transition-timing-function" : "cubic-bezier(.83,0,.22,1)" } );
+        }
+    });
+
+    // しおりが傾くアニメーション (左側たち)
+    $('.salaryspimg').each(function(){
+        var yy = $(this).offset().top+hh/2;//効果発生開始タイミングを操作したい場合は数値を変更する
+        if (tt > yy - hh){
+            $(this).css( { transform: "rotate(-7deg)" } );
+            $(this).css( { "transition-timing-function" : "cubic-bezier(.83,0,.22,1)" } );
+        }
+    });
+    $('.nursespchara').each(function(){
+        var yy = $(this).offset().top+hh/2;//効果発生開始タイミングを操作したい場合は数値を変更する
+        if (tt > yy - hh){
+            $(this).css( { transform: "rotate(-7deg)" } );
+            $(this).css( { "transition-timing-function" : "cubic-bezier(.83,0,.22,1)" } );
+        }
+    });
+    $('.busgirlspimg').each(function(){
+        var yy = $(this).offset().top+hh/2;//効果発生開始タイミングを操作したい場合は数値を変更する
+        if (tt > yy - hh){
+            $(this).css( { transform: "rotate(-7deg)" } );
+            $(this).css( { "transition-timing-function" : "cubic-bezier(.83,0,.22,1)" } );
         }
     });
 }
